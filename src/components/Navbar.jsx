84fe6e7fd9410c0069ecd8a171eeb4face048709
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-01-18 19:53:48 +0530

    update nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,16 @@ export const Navbar = () => {
                 {/* <a href=""><img src="fevicon.png"  className='size-16' alt="" /></a> */}
             </div>
             <div className='flex gap-7 justify-center m-8 items-center'>
-                <a href="https://instagram.com/vishaaal05" className='invert' target='_blank' rel='noreferrer noopener' aria-label='Instagram'>
+                <a href="https://instagram.com/vishaaal05" className='' target='_blank' rel='noreferrer noopener' aria-label='Instagram'>
                     <FaInstagram className='text-3xl'/>
                 </a>
-                <a href="https://github.com/vishaaal05" className='invert' target='_blank' rel='noreferrer noopener' aria-label='Github'>
+                <a href="https://github.com/vishaaal05" className='' target='_blank' rel='noreferrer noopener' aria-label='Github'>
                     <FaGithub  className='text-3xl'/>
                 </a>
-                <a href="https://www.linkedin.com/in/vishaaal05/" className='invert' target='_blank' rel='noreferrer noopener' aria-label='LinkedIn'>
+                <a href="https://www.linkedin.com/in/vishaaal05/" className='' target='_blank' rel='noreferrer noopener' aria-label='LinkedIn'>
                     <FaLinkedin className='text-3xl'/>
                 </a>
-                <a href="https://x.com/Vishaaal05" className='invert' target='_blank' rel='noreferrer noopener' aria-label='Twitter'>
+                <a href="https://x.com/Vishaaal05" className='' target='_blank' rel='noreferrer noopener' aria-label='Twitter'>
                     <FaXTwitter className='text-3xl'/>
                 </a>
             </div>
