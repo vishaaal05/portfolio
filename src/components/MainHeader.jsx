c8b3aa0bf10598e6d5b618d9cffaca353bc6d219
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-03-21 19:36:06 +0530

    update framer-motion to version 12.5.0 and add animations to MainHeader component

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,31 +1,30 @@
-import React from "react";
+import { motion } from "framer-motion";
 
 export const MainHeader = () => {
   return (
     <div className="flex flex-col md:flex-row justify-between items-center py-16 md:py-36">
-      {/* Image Section */}
-      <div className="px-5 md:px-20 mb-8 md:mt-0 flex justify-center order-0 md:order-2 ">
-        {" "}
-        {/* Set order to 1 for mobile, 0 for desktop */}
+      <motion.div 
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+        className="px-5 md:px-20 mb-8 md:mt-0 flex justify-center order-0 md:order-2">
         <div>
           <img
             src="profile-removebg-preview.jpg"
             alt="Vishal Kumar Gupta - Profile"
-            className="rounded-3xl max-w-full md:max-w-80"
+            className="rounded-3xl max-w-full md:max-w-80 hover:scale-105 transition-transform duration-300"
           />
         </div>
-      </div>
+      </motion.div>
 
-      {/* Text Section */}
-      <div className="px-5 md:px-20 w-full md:w-1/2 text-white order-0 md:order-1">
-        {" "}
-        {/* Set order to 0 for mobile, 1 for desktop */}
+      <motion.div 
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="px-5 md:px-20 w-full md:w-1/2 text-white order-0 md:order-1">
         <h1 className='text-3xl md:text-5xl py-2 md:py-1 text-center md:text-left tracking-tighter text-white'>Vishal Kumar Gupta</h1>
         <h3 className='text-xl md:text-2xl py-2 md:py-7 text-center md:text-left bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text tracking-tight text-transparent'>Full Stack Developer</h3>
         <p className='leading-7'>I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.</p>
-        {/* <div className="text-left mt-4">
-          <button className="bg-white text-black p-2 rounded-lg">Download CV</button>
-        </div> */}
         <div className="mt-10 text-center md:text-left">
           <a
             href="/resume.pdf"
@@ -36,7 +35,7 @@ export const MainHeader = () => {
             Download CV
           </a>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
